8c86c1188e632b49f0a4203e8798ac9ba6e2ef51
장강민 <jkm9780533@gmail.com>
2025-11-12 10:40:54 +0900

    feat: shop_study 카드 3개 추가

---
+++ shop_study/src/App.jsx
@@ -0,0 +1,42 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Navbar, Nav, Container, Row, Col } from "react-bootstrap";
+import { useState } from "react";
+import data from "./data";
+
+let Shoes = (props) => {
+    return (
+        <Col md={"4"}>
+            <img src={"/shoes" + (props.i+1) + ".png"} width="80%"></img>
+            <h4>{props.shoes.title}</h4>
+            <p>{props.shoes.price}</p>
+        </Col>
+    );
+};
+function App() {
+    let [shoes, setshoes] = useState(data);
+    return (
+        <div className="body">
+            <Navbar bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand href="#home">어쩔티비</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">cart</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+            <div className="main-bg"></div>
+            <Row>
+                {/* <Shoes shoes={shoes[1]} i={2} />
+                <Shoes shoes={shoes[2]} i={3} /> */}
+                {shoes.map((a, i) => {
+                    return <Shoes shoes={shoes[i]} i={i} />;
+                })}
+            </Row>
+        </div>
+    );
+}
+
+export default App;
