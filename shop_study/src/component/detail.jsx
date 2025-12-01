f2586f8e05b787d4083f6ab4f2916ab53fce0f78
장강민 <jkm9780533@gmail.com>
2025-12-01 20:56:30 +0900

    useEffect 학습

--- shop_study/src/component/detail.jsx
+++ shop_study/src/component/detail.jsx
@@ -1,11 +1,43 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+
 const Detail = (props) => {
-  let {id} = useParams();
+
+  let [count, setCount] = useState(0)
+  let [Alert, setAlert] = useState(true)
+  let [input, setInput] = useState('')
+  let { id } = useParams();
   let product = props.shoes.find((x) => {
     return x.id == id
   });
+
+  useEffect(() => {
+    let time = setTimeout(() => { setAlert(false) }, 2000)
+    console.log(2)
+    return () => {
+      console.log(1)
+      clearTimeout(time)
+    }
+  })
+
+  useEffect(() => {
+    if (isNaN(input) == true){
+      alert('이건좀아니지라')
+    }
+  }, [input])
+
   return (
     <div className="container">
+      {
+        Alert == true
+          ? <div className="alert alert-warning">
+            2초이내 구매시 할인
+          </div>
+          : null
+      }
+
+      {count}
+      <button onClick={() => { setCount(count + 1) }}>버튼</button>
       <div className="row">
         <div className="col-md-6">
           <img
@@ -14,6 +46,7 @@ const Detail = (props) => {
           />
         </div>
         <div className="col-md-6">
+          <input onChange={(e) => { setInput(e.target.value) }}></input>
           <h4 className="pt-5">{product.title}</h4>
           <p>{product.content}</p>
           <p>{product.price} 원</p>
