054b1c756815ccef0ff20d5dcc42e0977f5b8f0f
장강민 <jkm9780533@gmail.com>
2025-12-22 23:08:03 +0900

    redux1 학습

--- shop_study/src/component/Crat.jsx
+++ shop_study/src/component/Crat.jsx
@@ -1,6 +1,10 @@
 import { Table } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 const Cart = () => {
+
+  let state = useSelector((state) => { return state })
+
   return (
     <div>
       <Table>
@@ -13,12 +17,18 @@ const Cart = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>안녕</td>
-            <td>안녕</td>
-            <td>안녕</td>
-          </tr>
+          {
+            state.cart.map((a, i) => {
+              return (
+                <tr key={i}>
+                  <td>{state.cart[i].name}</td>
+                  <td>{state.cart[i].count}</td>
+                  <td>안녕</td>
+                  <td>안녕</td>
+                </tr>
+              )
+            })
+          }
         </tbody>
       </Table>
     </div>
--- shop_study/src/main.jsx
+++ shop_study/src/main.jsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import store from './store.js'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
-  <Provuder store={store}>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provuder>
+  </Provider>
 )
