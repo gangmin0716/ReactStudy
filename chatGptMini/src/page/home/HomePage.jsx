dd93b5c39a740f0613a5cffe3a264455f244ad45
장강민 <jkm9780533@gmail.com>
2025-12-27 20:07:40 +0900

    feat: 홈배경 추가and  파일구성

---
+++ chatGptMini/src/page/home/HomePage.jsx
@@ -0,0 +1,9 @@
+import * as S from "./style/home"
+
+export default function Home() {
+  return (
+    <S.Frame>
+      진짜 나뿌다
+    </S.Frame>
+  )
+}
---
+++ chatGptMini/src/page/home/style/home.js
@@ -0,0 +1,6 @@
+import styled from "styled-components"
+
+export const Frame = styled.div`
+  background-color: #212121;
+  height: 100vh;
+`;
