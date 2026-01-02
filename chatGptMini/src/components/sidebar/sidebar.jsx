6d308441f9dd3ff00418ce38044d41bf999b9420
장강민 <jkm9780533@gmail.com>
2026-01-02 18:20:20 +0900

    feat: sidebar 상단부분 icon추가

--- chatGptMini/src/components/sidebar/sidebar.jsx
+++ chatGptMini/src/components/sidebar/sidebar.jsx
@@ -1,8 +1,15 @@
 import * as S from "./style/sidebar"
+import ChatGPTIcon from '@/assets/ChatGPT.svg?react';
+import SidebarIcon from '@/assets/Sidebar.svg?react'
 
 export default function Sidebar() {
-  return(
+  return (
     <S.Frame>
+      <S.Top>
+        <S.ChatGPTIcon></S.ChatGPTIcon>
+        <S.SidebarIcon></S.SidebarIcon>
+      </S.Top>
     </S.Frame>
   )
 }
+
--- chatGptMini/src/components/sidebar/style/sidebar.js
+++ chatGptMini/src/components/sidebar/style/sidebar.js
@@ -1,6 +1,34 @@
 import { styled } from "styled-components";
+import ChatGPTSvg from '@/assets/ChatGPT.svg?react';
+import SidebarSvg from '@/assets/Sidebar.svg?react';
 
 export const Frame = styled.div`
   width: 20%;
   background-color: #181818;
+`;
+
+export const Top = styled.div`
+  padding: 14px 14px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ChatGPTIcon = styled(ChatGPTSvg)`
+  width:26px;
+  height: 26px;
+`;
+
+export const SidebarIcon = styled(SidebarSvg)`
+  width: 19px;
+  height: 19px;
+  cursor: pointer;
+
+  path {
+    stroke: #afafaf;
+  }
+
+  &:hover path {
+    stroke: white;
+  }
 `;
