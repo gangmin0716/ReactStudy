c94dab4016ed933b3f4c5c0efd06027371b03e7f
장강민 <jkm9780533@gmail.com>
2026-01-05 19:54:32 +0900

    Text3

--- winter/src/component/Test3.jsx
+++ winter/src/component/Test3.jsx
@@ -1,24 +1,26 @@
 import { useState } from "react";
 
-const Lists = [
-  "qwfqwf", "qwfpojqwfwpoj"
-]
-
 const Test3 = () => {
   const [text, setText] = useState("");
+  const [list, setList] = useState([]);
+
   const onChange = (e) => {
     setText(e.target.value)
+  };
+
+  const onClick = (e) => {
+    setList([...list, e])
+    setText("")
   }
   return (
     <>
       <input type="text" placeholder="내용 입력" value={text} onChange={onChange} style={{ border: "1px solid black" }} />
-      <button>추가</button>
+      <button onClick={() => onClick(text)}>추가</button>
       {
-        Lists.map((List, index) => {
-          return(
-            
+        list.map((a, i) => (
+            <li key={i}>{a}</li>
           )
-        })
+        )
       }
     </>
   )
