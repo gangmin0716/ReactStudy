ee1052ea3513ce0db673b65da9f4857944ce7b1f
장강민 <jkm9780533@gmail.com>
2026-01-05 20:14:26 +0900

    Test4

--- winter/src/App.jsx
+++ winter/src/App.jsx
@@ -1,13 +1,14 @@
 import Test1 from './component/test1'
 import Test2 from './component/Test2'
 import Test3 from './component/Test3'
+import Test4 from './component/Test4'
 import "./firebase/firebace"
 import './App.css'
 
 function App() {
   return (
     <>
-      <Test3 />
+      <Test4 />
     </>
   )
 }
