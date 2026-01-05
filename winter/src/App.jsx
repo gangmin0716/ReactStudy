9cee769301b49fc1f1b078fedf80eb4ebd83e271
장강민 <jkm9780533@gmail.com>
2026-01-05 20:27:02 +0900

    switch testComponents

--- winter/src/App.jsx
+++ winter/src/App.jsx
@@ -1,15 +1,10 @@
-import Test1 from './component/test1'
-import Test2 from './component/Test2'
-import Test3 from './component/Test3'
-import Test4 from './component/Test4'
-import Test5 from './component/Test5'
 import "./firebase/firebace"
 import './App.css'
 
 function App() {
   return (
     <>
-      <Test5 />
+
     </>
   )
 }
