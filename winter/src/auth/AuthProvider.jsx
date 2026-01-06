b28f10daa38e1143458619c90a34f8765b53c769
장강민 <jkm9780533@gmail.com>
2026-01-06 14:03:19 +0900

    useEffect

--- winter/src/auth/AuthProvider.jsx
+++ winter/src/auth/AuthProvider.jsx
@@ -1,13 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import {auth} from '../firebase/firebace';
 import { AuthContext } from './AuthContext';
 
 export default function AuthProvider({ children }) {
-const [user, setUser] = useState(null);
-const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-return (
-<AuthContext.Provider value={{ user, loading, setUser, setLoading }}>
-{children}
-</AuthContext.Provider>
-);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    })
+    return () => {
+      unsubscribe(false);
+    }
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ user, loading, setUser, setLoading }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
