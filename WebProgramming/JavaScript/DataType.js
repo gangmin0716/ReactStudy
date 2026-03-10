11bd4d6ceac8ab406e9dbce6e62592d3c4f8f2bc
장강민 <jkm9780533@gmail.com>
2026-03-10 11:08:04 +0900

    number01

---
+++ WebProgramming/JavaScript/DataType.js
@@ -0,0 +1,15 @@
+// 1. 지불금액(pay): 10000원, 물건가격(price): 1500원, 구매 개수(num) 5개일 때,
+//     거스름돈을 계산하는 프로그램을 작성하고 캡쳐하여 제출하세요.
+
+let pay = 10000;
+let num = 5;
+let price = 1500;
+let change = pay - price * num;
+
+console.log(change);
+
+// 2. 킬로미터를 마일로 환산하는 프로그램을 작성하고, 이를 캡쳐하여 제출하세요.
+
+let km = 5;
+let mile = km * 0.621371;
+console.log(mile);
