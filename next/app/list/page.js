6df715387140ef1274397eb87cb45666b19693a8
장강민 <jkm9780533@gmail.com>
2026-03-19 10:28:16 +0900

    try

---
+++ next/app/list/page.js
@@ -0,0 +1,15 @@
+export default function List() {
+
+    return (
+        <>
+            <h4 className="text-center text-xl font-medium mb-3 mt-70">상품목록</h4>
+
+            <div className="">
+                <h4>상품1 $40</h4>
+            </div>
+            <div className="">
+                <h4>상품2 $40</h4>
+            </div>
+        </>
+    )
+}
