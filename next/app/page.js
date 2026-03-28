6aaccecb17a69f20d097a0c0f578ab876e3706d6
장강민 <jkm9780533@gmail.com>
2026-03-28 15:52:17 +0900

    next init

--- next/app/page.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  let name = 'park'
-  return (
-    <div>
-      <h4 className="text-center text-xl font-medium mb-3 mt-70">후레시민트</h4>
-      <p className="text-base text-center">by dev {name}</p>
-    </div>
-  );
-}
