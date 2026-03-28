f5850cd373376946aad7bf969ce7988952e96899
장강민 <jkm9780533@gmail.com>
2026-03-28 17:06:08 +0900

    feat: cart

---
+++ next/app/cart/layout.js
@@ -0,0 +1,10 @@
+const Layout = ({children}) => {
+  return (
+    <div>
+      <p className="titleSub">현대카드 무이자이벤트중</p>
+      {children}
+    </div>
+  )
+}
+
+export default Layout
---
+++ next/app/cart/payment/page.js
@@ -0,0 +1,9 @@
+const payment = () => {
+  return (
+    <div>
+      <h1 className="title">결제페이지입니다</h1>
+    </div>
+  )
+}
+
+export default payment
