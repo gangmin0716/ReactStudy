141f59599eab24e9ac0408e7769a9e38c42703e7
장강민 <jkm9780533@gmail.com>
2026-03-31 20:36:07 +0900

    map

--- next/app/list/page.js
+++ next/app/list/page.js
@@ -1,14 +1,17 @@
 const list = () => {
+  let product = ["Tomatoes", "Pasta", "Coconut"];
   return (
     <div>
       <h4 className="title">상품목록</h4>
 
-      <div className="food">
-        <h4>상품1 $40</h4>
-      </div>
-      <div className="food">
-        <h4>상품2 $40</h4>
-      </div>
+      {product.map((item, index) => {
+        return (
+          <div key={index} className="food">
+            <img src= />
+            <h4>{item} $40</h4>
+          </div>
+        );
+      })}
     </div>
   );
 };
