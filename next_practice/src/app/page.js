34dde12375c9a346ef09a1c0a90838713874c588
장강민 <jkm9780533@gmail.com>
2026-04-06 21:30:39 +0900

    mongodb setting

--- next_practice/src/app/page.js
+++ next_practice/src/app/page.js
@@ -1,9 +1,15 @@
-const Home = () => {
+import { connectDB } from "@/util/database";
+
+const Home = async () => {
+  const client = await connectDB;
+  const db = client.db("forum");
+  let a =await db.collection("post").find().toArray();
+  console.log(a);
   return (
-      <div>
-        <h1>home</h1>
-      </div>
-  )
-}
+    <div>
+      <h1>home</h1>
+    </div>
+  );
+};
 
 export default Home;
