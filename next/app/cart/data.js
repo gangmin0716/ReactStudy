4382742bacca10a95ee2e5aeee862abfda7c8c65
장강민 <jkm9780533@gmail.com>
2026-04-06 17:21:00 +0900

    com

---
+++ next/app/cart/data.js
@@ -0,0 +1,17 @@
+const Item = () => {
+  return (
+    <div className="cart-item">
+      <p>상품명</p>
+      <p>$40</p>
+      <p>1개</p>
+    </div>
+  );
+};
+
+const H1 = () => {
+  return (
+    <h1 className="title">안녕하세요</h1>
+  )
+}
+
+export { Item, H1};
--- next/app/cart/page.js
+++ next/app/cart/page.js
@@ -1,9 +1,15 @@
-const cart = () => {
-  return(
-    <div>
-      <h1 className="title">장바구니 입니다</h1>
-    </div>
-  )
-}
+import { Item, H1 } from "./data";
 
-export default cart
+const Cart = () => {
+  return (
+    <div>
+      <H1 />
+      <h4 className="title">Cart</h4>
+      <Item />
+      <Item />
+      <Item />
+    </div>
+  );
+};
+
+export default Cart;
