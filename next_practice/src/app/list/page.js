371e6b1322ad83aa31f655642331606983565947
장강민 <jkm9780533@gmail.com>
2026-04-07 20:59:27 +0900

    add detail page and link from list to post

--- next_practice/src/app/list/page.js
+++ next_practice/src/app/list/page.js
@@ -1,22 +1,22 @@
 import { connectDB } from "@/util/database";
+import Link from "next/link";
 
 const List = async () => {
-
   const db = (await connectDB).db("forum");
   let result = await db.collection("post").find().toArray();
 
   return (
     <div className="list-bg">
-      {
-        result.map((item, index) => {
-          return (
+      {result.map((item, index) => {
+        return (
+          <Link key={index} href={`/detail/${result[index]._id.toString()}`}>
             <div key={index} className="list-item">
               <h4>{result[index].title}</h4>
               <p>{result[index].content}</p>
             </div>
-          );
-        })
-      }
+          </Link>
+        );
+      })}
     </div>
   );
 };
