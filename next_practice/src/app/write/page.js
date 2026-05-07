1f1b29e0f450077652ed2cea241dd6e14bff78dc
장강민 <jkm9780533@gmail.com>
2026-05-07 11:22:29 +0900

    넥스트, 리넥 학습

--- next_practice/src/app/write/page.js
+++ next_practice/src/app/write/page.js
@@ -1,9 +1,19 @@
+"use client";
+import { useState } from "react";
+
 const Write = () => {
+  const [input, setInput] = useState("");
   return (
     <div>
       <h4>글작성</h4>
-      <form action="/api/date" method="GET">
-        <input name="title" />
+      <form action="/api/write" method="POST">
+        <input
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+          value={input}
+          name="title"
+        />
         <button type="submit">버튼</button>
       </form>
     </div>
--- next_practice/src/pages/api/write.js
+++ next_practice/src/pages/api/write.js
@@ -1,1 +1,7 @@
-const handler = (request, response) => {};
+const handler = (request, response) => {
+  if (request.method === "POST") {
+    return response.json(response.body);
+  }
+};
+
+export default handler;
