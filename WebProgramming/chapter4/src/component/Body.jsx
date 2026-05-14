1c309750a5a430572c47b0269db981d36fe02f54
장강민 <jkm9780533@gmail.com>
2026-05-14 10:28:10 +0900

    useState option

--- WebProgramming/chapter4/src/component/Body.jsx
+++ WebProgramming/chapter4/src/component/Body.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 
 const Body = () => {
-  const [date, setDate] = useState("");
+  const [option, setOption] = useState("");
   const handleOnChange = (e) => {
     console.log("변경된 값: ", e.target.value)
-    setDate(e.target.value)
+    setOption(e.target.value)
   };
   return (
     <div>
-      <input type="date" onChange={handleOnChange} value={date} />
+      <select value={option} onChange={handleOnChange}>
+        <option key={"1번"}>1번</option>
+        <option key={"2번"}>2번</option>
+        <option key={"3번"}>3번</option>
+      </select>
     </div>
   );
 };
