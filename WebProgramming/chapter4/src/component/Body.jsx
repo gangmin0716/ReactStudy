6d217d7e8b8e9875014098cb2b94a3a5c52b5af0
장강민 <jkm9780533@gmail.com>
2026-05-14 10:14:12 +0900

    event

--- WebProgramming/chapter4/src/component/Body.jsx
+++ WebProgramming/chapter4/src/component/Body.jsx
@@ -1,9 +1,15 @@
-const Body = ({children}) => {
+const Body = () => {
+
+  const handleOnClick = (ev) => {
+    console.log(ev.target.name)
+  }
+
   return (
-      <div>
-        {children}
-      </div>
-  )
+    <div>
+      <button name="A버튼" onClick={handleOnClick}>A버튼</button>
+      <button name="B버튼" onClick={handleOnClick}>B버튼</button>
+    </div>
+  );
 }
 
 export default Body
