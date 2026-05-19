65f4f7d8876a309423620ba7e3e41caff4d3d393
장강민 <jkm9780533@gmail.com>
2026-05-19 11:35:47 +0900

    카운터

--- WebProgramming/chapter4/src/component/Body.jsx
+++ WebProgramming/chapter4/src/component/Body.jsx
@@ -1,55 +1,32 @@
-import { useState } from "react"
+import { useState } from "react";
 
-function Body(){
-  const [state, setState] = useState({
-    userName: "",
-    gender: "",
-    birth: "",
-    bio: ""
-  }) // 객체 자료형으로 state 관리
-
-  const handleOnChange = (e) => {
-    const { name, value } = e.target
-
-    console.log("현재 수정 대상: ", name)
-    console.log("수정 값: ", value)
-
-    setState({
-      ...state,
-      [name]: value
-    })
-  }
-
-  return( // 인풋 박스 등등... 화면에 렌더링될 것들
-    <div>
-      <div>
-        <input
-          name="userName"
-          value={state.userName}
-          onChange={handleOnChange}
-          placeholder="이름"
-        />
-      </div>
-      <div>
-        <select name="gender" value={state.gender} onChange={handleOnChange}>
-          <option value=""></option>
-          <option value="남성">남성</option>
-          <option value="여성">여성</option>
-        </select>
-      </div>
-      <div>
-        <input
-          name="birth"
-          type="date"
-          value={state.birth}
-          onChange={handleOnChange}
-        />
-      </div>
-      <div>
-        <textarea name="bio" value={state.bio} onChange={handleOnChange} />
-      </div>
-    </div>
-  )
+function Viewer({ number }) {
+  // State 변수 number 전달
+  return <div>{number % 2 === 0 ? <h3>짝수</h3> : <h3>홀수</h3>}</div>;
 }
 
-export default Body
+function Body() {
+  const [number, setNumber] = useState(0);
+
+  const onIncrease = () => {
+    setNumber(number + 1);
+  };
+
+  const onDecrease = () => {
+    setNumber(number - 1);
+  };
+
+  return (
+    <div>
+      <h2>{number}</h2>
+      <Viewer number={number}></Viewer>
+
+      <div>
+        <button onClick={onDecrease}>-</button>
+        <button onClick={onIncrease}>+</button>
+      </div>
+    </div>
+  );
+}
+
+export default Body;
