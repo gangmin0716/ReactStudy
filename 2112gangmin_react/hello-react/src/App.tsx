9f0236ef7ed5fc56a10e72e9df19ffae2ddc83b2
장강민 <jkm9780533@gmail.com>
2026-08-27 16:06:39 +0900

    props 실습2

--- 2112gangmin_react/hello-react/src/App.tsx
+++ 2112gangmin_react/hello-react/src/App.tsx
@@ -4,6 +4,12 @@ type StudentCardProps = {
   name: string;
   school?: string;
 }
+
+type Bookprops = {
+  title: string;
+  author: string;
+}
+
 const StudentCard = (props: StudentCardProps) => {
   return (
     <div>
@@ -14,10 +20,21 @@ const StudentCard = (props: StudentCardProps) => {
   )
 }
 
+const BookCard = (props: Bookprops) => {
+  return (
+    <div>
+      <h2>책 카드</h2>
+      <p>이름: {props.title}</p>
+      <p>작가: {props.author}</p>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div>
       <StudentCard name="장강민" school="대구소프트웨어마이스터고등학교" />
+      <BookCard title="대소고에서 살아 남기" author="강장민"/>
     </div>
   );
 }
