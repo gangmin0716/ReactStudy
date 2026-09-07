27187630b0acd4aa7b96089baec626ee85dd6f98
장강민 <jkm9780533@gmail.com>
2026-09-07 11:50:46 +0900

    counter

--- 2112gangmin_react/hello-react/src/App.tsx
+++ 2112gangmin_react/hello-react/src/App.tsx
@@ -1,18 +1,41 @@
 import "./App.css";
 import { useState } from "react";
-function App() {
-  const [numbers, setNumbers] = useState<number[]>([]);
-  const handleAdd = () => {
-    setNumbers([...numbers, numbers.length]); // spread 연산자로 새 배열 만들기
-    console.log(numbers);
-  };
+
+type OperationType = "add" | "reset";
+type ButtonsProps = {
+  updateCount: (type: OperationType) => void;
+};
+
+const Buttons = ({ updateCount }: ButtonsProps) => {
   return (
     <>
-      {numbers.map(it => (
-        <p>{it}</p>
-      ))}
-      <button onClick={handleAdd}>Add</button>
+      <button onClick={() => updateCount("add")}>+</button>
+      <button onClick={() => updateCount("reset")}>RESET</button>
     </>
   );
-}
+};
+
+type CountProps = {
+  count: number;
+};
+
+const Count = ({ count }: CountProps) => {
+  return <h1>{count}</h1>;
+};
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const updateCount = (type: OperationType) => {
+    if (type === "add") setCount(count + 1);
+    else if (type === "reset") setCount(0);
+  };
+
+  return (
+    <>
+      <Count count={count} />
+      <Buttons updateCount={updateCount} />
+    </>
+  );
+};
+
 export default App;
