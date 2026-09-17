b3045c801f1093113179be75e238e90df567ece2
장강민 <jkm9780533@gmail.com>
2026-09-17 14:39:45 +0900

    css 적용한 전공 소개

--- 2112gangmin_react/hello-react/src/App.tsx
+++ 2112gangmin_react/hello-react/src/App.tsx
@@ -1,10 +1,40 @@
-import "./App.css";
-import reactLogo from "./assets/react.svg";
-export default function App() {
+import jqueryLogo from "./assets/jquery.png";
+
+function App() {
+  const myStyle = {
+    color: "white",
+    backgroundColor: "dodgerblue",
+    padding: "10px",
+    fontFamily: "Verdana",
+  };
+
+  const iWidth = { maxWidth: "300px" };
   return (
     <>
-      <img src="/favicon.svg"></img>
-      <img src={reactLogo}></img>
+      <h2>React-Style</h2>
+      <ol>
+        <li>프론트엔드</li>
+        <ul>
+          <li>
+            <img src="/img/html_css_js.png" style={iWidth} />
+          </li>
+          <li>
+            <img src={jqueryLogo} style={iWidth} />
+          </li>
+          <li>
+            <img src="http://nakja.co.kr/images/reactjs.png" style={iWidth} />
+          </li>
+        </ul>
+        <li>
+          백엔드
+          <ul>
+            <li id="backEndSub">Java</li>
+            <li className="warnings">Oracle</li>
+            <li style={myStyle}>JSP</li>
+          </ul>{" "}
+        </li>
+      </ol>
     </>
   );
 }
+export default App;
